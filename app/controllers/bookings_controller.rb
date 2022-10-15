9409c0f17381ef3fe40465c461762877d904ab43
Newton <newtieblackie@gmail>
2022-10-15 15:19:05 +0300

    Set up of the Booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,51 +1,18 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: %i[ show update destroy ]
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unproccessable_entity_response
 
-  # GET /bookings
-  def index
-    @bookings = Booking.all
-
-    render json: @bookings
-  end
-
-  # GET /bookings/1
-  def show
-    render json: @booking
-  end
-
-  # POST /bookings
   def create
-    @booking = Booking.new(booking_params)
-
-    if @booking.save
-      render json: @booking, status: :created, location: @booking
-    else
-      render json: @booking.errors, status: :unprocessable_entity
-    end
+      booking = Booking.create!(attendee_params)
+      render json:{success:'Successfully submitted.'}, status: :created
   end
-
-  # PATCH/PUT /bookings/1
-  def update
-    if @booking.update(booking_params)
-      render json: @booking
-    else
-      render json: @booking.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /bookings/1
-  def destroy
-    @booking.destroy
-  end
-
+  
+  
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_booking
-      @booking = Booking.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def booking_params
-      params.require(:booking).permit(:fullname, :email, :pick_up_location, :phone_number, :number_of_slots, :event_id)
-    end
+  def booking_params
+      params.permit(:fullname, :email, :pick_up_location, :phone_number ,:number_of_slots, :event_id)
+  end
+  
+  def render_unproccessable_entity_response(invalid)
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,5 @@
 class Booking < ApplicationRecord
+    validates :fullname , :email , :pick_up_location ,:number_of_slots
+
+    has_many :events
 end
