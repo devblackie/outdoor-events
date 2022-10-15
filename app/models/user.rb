0a7127d8cb657623d035e4eea79450cfedc6ee19
Newton <newtieblackie@gmail>
2022-10-15 16:02:39 +0300

    Added validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, presence: true , uniqueness: true
+    validates :password, presence: true , length: { in: 7..11 }
+
+    has_many :events
+
+
 end
+
