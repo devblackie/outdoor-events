822b529b64cadcaee965a9eecb93640579d11491
Newton <newtieblackie@gmail>
2022-10-15 13:33:32 +0300

    Set up the user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   get 'gem/install'
   get 'gem/faker'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
