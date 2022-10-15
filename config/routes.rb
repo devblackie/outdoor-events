9717662e819ddfd3052bdf888cbc69f9d746c169
Newton <newtieblackie@gmail>
2022-10-15 16:07:01 +0300

    Added the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   resources :events
   resources :bookings
   resources :users
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
   get 'gem/install'
   get 'gem/faker'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/hello', to: 'application#hello_world'
+  # get '/hello', to: 'application#hello_world'
 end
