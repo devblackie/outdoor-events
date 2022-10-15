5275926e88d7f301194c6f97962d4e86a6c6365c
Newton <newtieblackie@gmail>
2022-10-15 15:51:50 +0300

    Added the requiring entity for the booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
-    validates :fullname , :email , :pick_up_location ,:number_of_slots
+    validates :fullname , :email , :pick_up_location ,:number_of_slots ,presence: true
 
     has_many :events
 end
