a891362ad9a341b083e38dd5649f5f60ed1c40b4
Newton <newtieblackie@gmail>
2022-10-15 16:24:03 +0300

    Homestretch with the backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'start'
+User.create(
+    username:'koko',
+    password:'koko'
+)
+10.times do
+    Event.create(
+    eventname: Faker::TvShows::GameOfThrones.house,
+    fee: [500,200,100,10,150,600,1000,400].sample,
+    location: Faker::Nation.capital_city,
+    date: Faker::Date.forward(days: 23),
+    slots: rand(1..15),
+    details: Faker::Books::Lovecraft.paragraph(sentence_count: 2),
+    user_id: rand(1..8)
+    )
+end
+puts 'end'
