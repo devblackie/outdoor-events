f0a4c4237a296d0c926b729ef92c1f9fba316a31
Newton <newtieblackie@gmail>
2022-10-15 13:18:05 +0300

    installed the faker gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'gem/install'
+  get 'gem/faker'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
